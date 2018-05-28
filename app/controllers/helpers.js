e8f4ee1492e2a1e3f571507856a4394a6c6939d1
susyArenita <39687641+susyArenita@users.noreply.github.com>
2018-05-28 15:01:41 -0500

    Update helpers.js

--- app/controllers/helpers.js
+++ app/controllers/helpers.js
@@ -4,7 +4,7 @@ exports.index = function(req, res) {
         console.log('GET /');
         console.log(req.user);
         console.log(req.user.cat_roles_idRol);
-        db.query("select NombreRol from cat_roles where idRol=1", function(err, rows){
+        db.query("select NombreRol from cat_roles where idRol=?",[req.user.cat_roles_idRol], function(err, rows){
             var rol = JSON.parse(JSON.stringify(rows));
             //console.log(rol);
             if(rol[0].NombreRol == 'coordinador'){
