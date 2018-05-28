f6a763e1926c6f9bfc8761a396ee71cd8587242e
susyArenita <39687641+susyArenita@users.noreply.github.com>
2018-05-28 14:43:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride());
 app.use(methodOverride("_method"));
-app.use((req, res, next) => {
-  console.log(`${req.url} -${req.method}`);
-  next();
-});
 
 //static files
 app.use(express.static(path.join(__dirname, 'app/assets')));
