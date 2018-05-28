08af19a04a3c395ca2af1f9523686d1732920cdf
susyArenita <39687641+susyArenita@users.noreply.github.com>
2018-05-28 06:42:00 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 module.exports = mysql.createConnection({
   host:'bt59wny7g-mysql.services.clever-cloud.com',
-  user: '	ufwil18hitlhwggg',
+  user: 'ufwil18hitlhwggg',
   password: 'gvZfkzuRsOjkch2S44t',
   database: 'bt59wny7g',
   port: 3306
